ce24aba4752914cd628c29b3a3369caef39d2ee7
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2022-09-01 19:03:23 -0300

    teste container

--- src/main/java/demo/main.java
+++ src/main/java/demo/main.java
@@ -3,13 +3,18 @@ package demo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.print.Doc;
+
 public class main {
 
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-        Staff staff = context.getBean(Nurse.class);
+        Staff staff = context.getBean(Doctor.class);
         staff.assist();
+
+
+
     }
 }
